5338f5764a1d32b5d908e87e98168777eba487ba
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-01-29 14:57:59 +0100

    oploikujyhgrtf

---
+++ index3.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index3.php?dateDebut=2/05/2016&dateFin=27/11/2016">lien</a>
+</body>
+</html>
